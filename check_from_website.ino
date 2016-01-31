ad8de176708e55813291b6038ccb76c25a67eb2a
Amanda Ervin <amanda.ervin@ymail.com>
2016-01-31 14:01:42 -0500

    Create check_from_website.ino

---
+++ check_from_website.ino
@@ -0,0 +1,49 @@
+int value=0;
+
+void upload_stuff() {  
+  delay(1000);
+  ++value;
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  sensorVal = analogRead(sensorPin);
+  Serial.println(sensorVal);
+  // We now create a URI for the request
+  url = "/?";
+  
+  url += "sensorID=";
+  url += sensorID;
+  url += "&sensorVal=";
+  url += sensorVal;
+  
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  delay(10);
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  
+  Serial.println();
+  Serial.println("closing connection");
+}
+
+  
+  
+
+  
